9233567ffe5f40b4b54334251b93836cf519ddda
Nova-Strife <68599948+Nova-Strife@users.noreply.github.com>
2020-09-01 20:17:50 +0600

    Добавьте файлы проекта.

---
+++ MSP_Exam/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MyLibrary;
+
+namespace MSP_Exam
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Weather[] Weathers = new Weather[]
+        {
+            new Weather("Облачно",20.4),
+            new Weather("Солнечно",30.1),
+            new Weather("Холод",-15),
+            new Weather("Осадки",-10.8),
+            new Weather("Снег",-20.4),
+        };
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        readonly Random r = new Random();
+        private void ShowWeather_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(tbCity.Text))
+            {
+                MessageBox.Show("Введите город");
+                return;
+            }
+            blockInfo.Text = Weathers[r.Next(Weathers.Length)].ToString();
+        }
+    }
+}
---
+++ MSP_Exam/Weather.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSP_Exam
+{
+    public class Weather
+    {
+        public string Name { get; set; }
+        public double Temperature { get; set; }
+        public Weather(string name,double temp)
+        {
+            Name = name;
+            Temperature = temp;
+        }
+        public override string ToString()
+        {
+            return $"Погода: {Name}\nТемпература: {Temperature}";
+        }
+    }
+}
